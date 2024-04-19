36c38de2311bc4e673b8670c9130f5e68c559453
Moaazsalahttt <113786750+Moaazsalahttt@users.noreply.github.com>
2024-04-19 23:08:44 +0200

    Update Chatbot.js

--- plugins/Chatbot.js
+++ plugins/Chatbot.js
@@ -28,33 +28,3 @@ let vn = './STK-20240419-WA0198.webp'
  }; 
 
  export default handler;
-let handler = m => m; 
-
- handler.all = async function (m) { 
- let chat = global.db.data.chats[m.chat]; 
- let responses; 
- if (/^فينوم$/i.test(m.text)) { 
- responses = [ 
- 'تحت امرك اطلب؟'
- ]; 
- } else if (/^بوووت|بموووووووووت|بوت$/i.test(m.text)) { 
-     responses = [ 
-'✨تحت امرك فينوم موجود '
-     ]; 
-   } else if (/^اوامر$/i.test(m.text)) { 
-     responses = [ 
-       '*لا تنسى ال .*',  
-     ];
- }else if (/^تست$/i.test(m.text)) { 
-     responses = [ 
-       '😉✨شغال شغال تست فينوم مسيطر متقلقش',  
-     ];
- }
- if (responses) { 
- let randomIndex = Math.floor(Math.random() * responses.length); 
- conn.reply(m.chat, responses[randomIndex], m); 
- } 
- return !0 
- }; 
-
- export default handler;
